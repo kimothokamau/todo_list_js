a8762a08f955440b39b9eead0977e69ca883acdc
kimothokamau <kimothokamau@gmail.com>
2021-07-13 15:48:52 -0600

    Add automatic output names

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = ('To DO List');
 
   return element;
