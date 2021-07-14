c0ed78a4994fa399da6da35d82b74901bd1fa526
kimothokamau <kimothokamau@gmail.com>
2021-07-14 06:06:31 -0600

    Remove empty lines

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 import './style.css';
-// Create a "close" button and append it to each list item
-// const myNodelist = document.getElementsByTagName('li');
-
-// for (let i = 0; myNodelist.length; i++) {
-//   const span = document.createElement('span');
-//   const txt = document.createTextNode(' ');
-//   span.className = 'close';
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-
-// Add a "checked" class when clicking on a list item
 const list = document.querySelector('ul');
 list.addEventListener('click', (event) => {
   if (event.target.tagname === 'li') {
