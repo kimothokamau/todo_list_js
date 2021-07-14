2dbad9ff22fa50fc784c70b97c4e9391988031f7
kimothokamau <kimothokamau@gmail.com>
2021-07-14 06:05:31 -0600

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-/* eslint-disable no-plusplus */
 import './style.css';
 // Create a "close" button and append it to each list item
-const myNodelist = document.getElementsByTagName('li');
+// const myNodelist = document.getElementsByTagName('li');
 
-for (let i = 0; myNodelist.length; i++) {
-  const span = document.createElement('span');
-  const txt = document.createTextNode(' ');
-  span.className = 'close';
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+// for (let i = 0; myNodelist.length; i++) {
+//   const span = document.createElement('span');
+//   const txt = document.createTextNode(' ');
+//   span.className = 'close';
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
 
 // Add a "checked" class when clicking on a list item
 const list = document.querySelector('ul');
